93561c3945162d3b903ed0a21211c2a125c05774
taha-bagheri <baghery78@gmail.com>
2023-11-27 20:53:14 +0100

    El codigo ha sido coreggido

--- src/hundir_la_flota/ClienteTCP.java
+++ src/hundir_la_flota/ClienteTCP.java
@@ -5,81 +5,91 @@ import java.net.*;
 import java.util.Scanner;
 
 public class ClienteTCP {
-    private Socket socketCliente = null;
-    private BufferedReader entrada = null;
-    private PrintWriter salida = null;
+	private Socket socketCliente = null;
+	private BufferedReader entrada = null;
+	private PrintWriter salida = null;
 
-    public ClienteTCP(String ip, int puerto) {
-        try {
-            socketCliente = new Socket(ip, puerto);
-            System.out.println("Conexión establecida: " + socketCliente);
-            entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-            salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
-        } catch (IOException e) {
-            System.err.printf("Imposible conectar con ip:%s / puerto:%d", ip, puerto);
-            System.exit(-1);
-        }
-    }
+	// Constructor que establece la conexión con el servidor y crea los flujos de
+	// entrada/salida
+	public ClienteTCP(String ip, int puerto) {
+		try {
+			socketCliente = new Socket(ip, puerto);
+			System.out.println("Conexión establecida: " + socketCliente);
+			entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+			salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
+		} catch (IOException e) {
+			System.err.printf("Imposible conectar con ip:%s / puerto:%d", ip, puerto);
+			System.exit(-1);
+		}
+	}
 
-    public void closeClienteTCP() {
-        try {
-            salida.close();
-            entrada.close();
-            socketCliente.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("-> Cliente Terminado");
-    }
+	// Cierra los flujos de entrada/salida y el socket al finalizar la conexión
+	public void closeClienteTCP() {
+		try {
+			salida.close();
+			entrada.close();
+			socketCliente.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("-> Cliente Terminado");
+	}
 
-    public void enviarMsg(String linea) {
-        salida.println(linea);
-    }
+	// Envía un mensaje al servidor
+	public void enviarMsg(String linea) {
+		salida.println(linea);
+	}
 
-    public String recibirMsg() {
-        String msg = "";
-        try {
-            msg = entrada.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return msg;
-    }
+	// Recibe un mensaje del servidor
+	public String recibirMsg() {
+		String msg = "";
+		try {
+			msg = entrada.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return msg;
+	}
 
-    public static void main(String[] args) throws IOException {
-        ClienteTCP canal = new ClienteTCP("localhost", 5555);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("¡Bienvenido al juego Hundir la Flota!");
-        System.out.println("Esperando a que el servidor inicie el juego...");
+	// Punto de entrada principal para iniciar el cliente
+	public static void main(String[] args) throws IOException {
+		// Crea una instancia de ClienteTCP, estableciendo la conexión con el servidor
+		ClienteTCP canal = new ClienteTCP("localhost", 5555);
+		Scanner scanner = new Scanner(System.in);
 
-        canal.recibirMsg(); // Espera hasta que el servidor inicie el juego
+		// Mensajes de bienvenida y espera hasta que el servidor inicie el juego
+		System.out.println("¡Bienvenido al juego Hundir la Flota!");
+		System.out.println("Esperando a que el servidor inicie el juego...");
+		canal.recibirMsg();
 
-        System.out.println("¡El juego ha comenzado! Ingresa las coordenadas para disparar.");
-        System.out.println("Por ejemplo, A3 representa la posición en la columna A, fila 3.");
+		System.out.println("¡El juego ha comenzado! Ingresa las coordenadas para disparar.");
+		System.out.println("Por ejemplo, A3 representa la posición en la columna A, fila 3.");
 
-        String linea;
-        do {
-            System.out.println("Esperando tu turno...");
-            linea = scanner.nextLine();
-            canal.enviarMsg(linea);
+		String linea;
+		do {
+			System.out.println("Esperando tu turno...");
+			linea = scanner.nextLine();
+			canal.enviarMsg(linea);
 
-            String respuesta = canal.recibirMsg();
-            System.out.println("Respuesta del servidor: " + respuesta);
+			// Recibe y muestra la respuesta del servidor
+			String respuesta = canal.recibirMsg();
+			System.out.println("Respuesta del servidor: " + respuesta);
 
-            if (respuesta.equals("Ganaste")) {
-                System.out.println("¡Felicidades! Has hundido todos los barcos. ¡Ganaste!");
-                break;
-            } else if (respuesta.equals("Agua") || respuesta.equals("Tocado")) {
-                // Muestra un mensaje según la respuesta del servidor
-                System.out.println(respuesta);
-            }
+			if (respuesta.equals("Ganaste")) {
+				System.out.println("¡Felicidades! Has hundido todos los barcos. ¡Ganaste!");
+				break;
+			} else if (respuesta.equals("Agua") || respuesta.equals("Tocado")) {
+				// Muestra un mensaje según la respuesta del servidor
+				System.out.println(respuesta);
+			}
 
-            System.out.println("Esperando el turno del servidor...");
+			System.out.println("Esperando el turno del servidor...");
 
-        } while (!linea.equals("Adiós"));
+		} while (!linea.equals("Adiós"));
 
-        System.out.println("Gracias por jugar. ¡Hasta luego!");
-        canal.closeClienteTCP();
-        scanner.close();
-    }
+		System.out.println("Gracias por jugar. ¡Hasta luego!");
+		// Cierra los recursos al finalizar
+		canal.closeClienteTCP();
+		scanner.close();
+	}
 }
--- src/hundir_la_flota/ServidorTCP.java
+++ src/hundir_la_flota/ServidorTCP.java
@@ -4,145 +4,164 @@ import java.io.*;
 import java.net.*;
 
 public class ServidorTCP {
-    private Socket socketCliente;
-    private ServerSocket socketServidor;
-    private BufferedReader entrada;
-    private PrintWriter salida;
-    private char[][] tablero;
+	private Socket socketCliente;
+	private ServerSocket socketServidor;
+	private BufferedReader entrada;
+	private PrintWriter salida;
+	private char[][] tablero;
 
-    public ServidorTCP(int puerto) {
-        this.socketCliente = null;
-        this.socketServidor = null;
-        this.entrada = null;
-        this.salida = null;
-        this.tablero = new char[10][10];
-        inicializarTablero();
+	// Constructor del servidor que inicializa las variables y espera conexiones
+	public ServidorTCP(int puerto) {
+		this.socketCliente = null;
+		this.socketServidor = null;
+		this.entrada = null;
+		this.salida = null;
+		this.tablero = new char[10][10];
+		inicializarTablero();
 
-        try {
-            socketServidor = new ServerSocket(puerto);
-            System.out.println("Esperando conexión...");
-            socketCliente = socketServidor.accept();
-            System.out.println("Conexión aceptada: " + socketCliente);
-            entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-            salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
-        } catch (IOException e) {
-            System.out.println("No puede escuchar en el puerto: " + puerto);
-            System.exit(-1);
-        }
-    }
+		try {
+			socketServidor = new ServerSocket(puerto);
+			System.out.println("Esperando conexión...");
+			socketCliente = socketServidor.accept();
+			System.out.println("Conexión aceptada: " + socketCliente);
+			entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+			salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
+		} catch (IOException e) {
+			System.out.println("No puede escuchar en el puerto: " + puerto);
+			System.exit(-1);
+		}
+	}
 
-    private void inicializarTablero() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                tablero[i][j] = ' ';
-            }
-        }
-        // Coloca los barcos en posiciones fijas para demostrar el concepto
-        tablero[0][0] = 'B';
-        tablero[1][0] = 'B';
-        tablero[2][0] = 'B';
-        tablero[3][0] = 'B';
-        tablero[4][0] = 'B';
-        tablero[5][0] = 'B';
-        tablero[6][0] = 'B';
-        tablero[7][0] = 'B';
-        tablero[8][0] = 'B';
-        tablero[9][0] = 'B';
-    }
+	// Inicializa el tablero con barcos en posiciones fijas
+	private void inicializarTablero() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				tablero[i][j] = ' ';
+			}
+		}
+		tablero[0][0] = 'B';
+		tablero[1][0] = 'B';
+		tablero[2][0] = 'B';
+		tablero[3][0] = 'B';
+		tablero[4][0] = 'B';
+		tablero[5][0] = 'B';
+		tablero[6][0] = 'B';
+		tablero[7][0] = 'B';
+		tablero[8][0] = 'B';
+		tablero[9][0] = 'B';
+	}
 
-    public void mostrarTablero() {
-        System.out.println("Tablero actual:");
-        System.out.println("  A B C D E F G H I J");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < 10; j++) {
-                System.out.print(tablero[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+	// Muestra el estado actual del tablero en la consola
+	public void mostrarTablero() {
+		System.out.println("Tablero actual:");
+		System.out.println("  A B C D E F G H I J");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i + " ");
+			for (int j = 0; j < 10; j++) {
+				System.out.print(tablero[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 
-    public void enviarTablero() {
-        // Enviar la representación del tablero al cliente
-        StringBuilder tableroStr = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                tableroStr.append(tablero[i][j]).append(" ");
-            }
-            tableroStr.append("\n");
-        }
-        salida.println(tableroStr.toString());
-    }
+	// Envia la representación del tablero al cliente
+	public void enviarTablero() {
+		StringBuilder tableroStr = new StringBuilder();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				tableroStr.append(tablero[i][j]).append(" ");
+			}
+			tableroStr.append("\n");
+		}
+		salida.println(tableroStr.toString());
+	}
 
-    public String verificarCoordenada(String coordenada) {
-        int fila = coordenada.charAt(0) - 'A';
-        int columna = Integer.parseInt(coordenada.substring(1));
+	// Verifica la coordenada recibida del cliente y devuelve la respuesta
+	public String verificarCoordenada(String coordenada) {
+	    // Verifica si la coordenada no está vacía
+	    if (coordenada != null && coordenada.length() > 0) {
+	        int fila = coordenada.charAt(0) - 'A';
+	        int columna = Integer.parseInt(coordenada.substring(1));
 
-        if (tablero[fila][columna] == 'B') {
-            tablero[fila][columna] = 'X';  // Marcar como acertado
-            return "¡Tocado!";
-        } else {
-            return "¡Agua!";
-        }
-    }
+	        if (fila >= 0 && fila < 10 && columna >= 0 && columna < 10) {
+	            if (tablero[fila][columna] == 'B') {
+	                tablero[fila][columna] = 'X';  // Marcar como acertado
+	                return "¡Tocado!";
+	            } else {
+	                return "¡Agua!";
+	            }
+	        } else {
+	            return "¡Coordenada inválida!";
+	        }
+	    } else {
+	        return "¡Coordenada vacía!";
+	    }
+	}
 
-    public void closeServidorTCP() {
-        try {
-            salida.close();
-            entrada.close();
-            socketCliente.close();
-            socketServidor.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("-> Servidor Terminado");
-    }
 
-    public void enviarMsg(String respuesta) {
-        salida.println(respuesta);
-    }
+	// Cierra todas las conexiones y termina el servidor
+	public void closeServidorTCP() {
+		try {
+			salida.close();
+			entrada.close();
+			socketCliente.close();
+			socketServidor.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("-> Servidor Terminado");
+	}
 
-    public String recibirMsg() {
-        String linea = "";
-        try {
-            linea = entrada.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return linea;
-    }
+	// Envia un mensaje al cliente
+	public void enviarMsg(String respuesta) {
+		salida.println(respuesta);
+	}
 
-    public static void main(String[] args) throws IOException {
-        ServidorTCP servidor = new ServidorTCP(5555);
-        jugarComoServidor(servidor);
-        servidor.closeServidorTCP();
-    }
+	// Recibe un mensaje del cliente
+	public String recibirMsg() {
+		String linea = "";
+		try {
+			linea = entrada.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return linea;
+	}
 
-    private static void jugarComoServidor(ServidorTCP servidor) {
-        System.out.println("¡El cliente se ha conectado! Esperando que vea el tablero...");
+	// Punto de entrada principal para iniciar el servidor
+	public static void main(String[] args) throws IOException {
+		ServidorTCP servidor = new ServidorTCP(5555);
+		jugarComoServidor(servidor);
+		servidor.closeServidorTCP();
+	}
 
-        servidor.mostrarTablero(); // Mostrar el tablero al cliente
-        servidor.enviarTablero(); // Enviar el tablero al cliente
-        servidor.enviarMsg("El juego ha comenzado. ¡Buena suerte!");
+	// Lógica principal para jugar como servidor
+	private static void jugarComoServidor(ServidorTCP servidor) {
+		System.out.println("¡El cliente se ha conectado! Esperando que vea el tablero...");
 
-        System.out.println("Tablero enviado al cliente. Esperando coordenadas...");
+		servidor.mostrarTablero(); // Mostrar el tablero al cliente
+		servidor.enviarTablero(); // Enviar el tablero al cliente
+		servidor.enviarMsg("El juego ha comenzado. ¡Buena suerte!");
 
-        do {
-            String coordenada = servidor.recibirMsg(); // Recibir coordenada del cliente
-            System.out.println("Coordenada recibida del cliente: " + coordenada);
+		System.out.println("Tablero enviado al cliente. Esperando coordenadas...");
 
-            String respuesta = servidor.verificarCoordenada(coordenada);
-            servidor.enviarMsg(respuesta); // Enviar respuesta al cliente
-            System.out.println("Respuesta enviada al cliente: " + respuesta);
+		do {
+			String coordenada = servidor.recibirMsg(); // Recibir coordenada del cliente
+			System.out.println("Coordenada recibida del cliente: " + coordenada);
 
-            if (respuesta.equals("¡Tocado!")) {
-                System.out.println("El cliente ha acertado. Esperando siguiente movimiento...");
-            } else if (respuesta.equals("¡Agua!")) {
-                System.out.println("El cliente ha fallado. Esperando siguiente movimiento...");
-            }
+			String respuesta = servidor.verificarCoordenada(coordenada);
+			servidor.enviarMsg(respuesta); // Enviar respuesta al cliente
+			System.out.println("Respuesta enviada al cliente: " + respuesta);
 
-        } while (!servidor.verificarCoordenada("").equals("¡Ganaste!")); // Puedes ajustar la condición de salida según tus necesidades
+			if (respuesta.equals("¡Tocado!")) {
+				System.out.println("El cliente ha acertado. Esperando siguiente movimiento...");
+			} else if (respuesta.equals("¡Agua!")) {
+				System.out.println("El cliente ha fallado. Esperando siguiente movimiento...");
+			}
 
-        System.out.println("¡El cliente ha ganado!"); 
-    }
+		} while (!servidor.verificarCoordenada("").equals("¡Ganaste!")); // Puedes ajustar la condición de salida según
+																			// tus necesidades
+
+		System.out.println("¡El cliente ha ganado!");
+	}
 }
